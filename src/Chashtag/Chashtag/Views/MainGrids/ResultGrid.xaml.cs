b71ec709a85b9cfb1c488acc5e5929ed52af2346
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-03-22 18:11:57 +0700

    add file views

---
+++ src/Chashtag/Chashtag/Views/MainGrids/ResultGrid.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chashtag.Views.MainGrids
+{
+    /// <summary>
+    /// Interaction logic for ResultGrid.xaml
+    /// </summary>
+    public partial class ResultGrid : UserControl
+    {
+        public ResultGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
