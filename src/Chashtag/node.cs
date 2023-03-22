5963e80e62663d718a65dba29b301d25602358ea
Ilhamgzzlr <ilhamakbar21feb@gmail.com>
2023-03-22 13:42:01 +0700

    bfs

---
+++ src/Chashtag/node.cs
@@ -0,0 +1,131 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TreasureHunt{
+    public class Node
+    {
+        private string name;
+        private Tuple<int, int> coor;
+        private bool visited;
+        private List<Tuple<int, int, string>> path;
+        private Tuple<int, int> U;
+        private Tuple<int, int> S;
+        private Tuple<int, int> T;
+        private Tuple<int, int> B;
+
+        public Node(string _name = "")
+        {
+            this.name = _name;
+            this.coor = new Tuple<int,int>(0,0);
+            this.visited = false;
+            this.path = new List<Tuple<int, int, string>>();
+            this.U = null;
+            this.S = null;
+            this.T = null;
+            this.B = null;
+        }
+        public Node(Node n)
+        {
+            this.name = n.getName();
+            this.coor = new Tuple<int,int>(n.getCoor().Item1, n.getCoor().Item2);
+            this.visited = false;
+            this.path = new List<Tuple<int, int, string>>();
+            this.U = n.getUtara();
+            this.S = n.getSelatan();
+            this.T = n.getTimur();
+            this.B = n.getBarat();
+        }
+        // GETTER
+        public Tuple<int, int, string> getInfo()
+        {
+            return new Tuple<int, int, string>(this.coor.Item1, this.coor.Item2, this.name);
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public Tuple<int, int> getCoor()
+        {
+            return this.coor;
+        }
+        public List<Tuple<int, int, string>> getPath()
+        {
+            return this.path;
+        }
+        public Tuple<int, int> getUtara()
+        {
+            return this.U;
+        }
+        public Tuple<int, int> getSelatan()
+        {
+            return this.S;
+        }
+        public Tuple<int, int> getTimur()
+        {
+            return this.T;
+        }
+        public Tuple<int, int> getBarat()
+        {
+            return this.B;
+        }
+        public bool isVisited(){
+            return this.visited;
+        }
+
+        // SETTER
+        public void setName(string _name)
+        {
+            this.name = _name;
+        }
+        public void setCoor(int x, int y)
+        {
+            this.coor = new Tuple<int, int>(x, y);
+        }
+        public void setUtara(int x, int y)
+        {
+            this.U = new Tuple<int, int>(x,y);
+        }
+        public void setSelatan(int x, int y)
+        {
+            this.S = new Tuple<int, int>(x,y);
+        }
+        public void setTimur(int x, int y)
+        {
+            this.T = new Tuple<int, int>(x,y);
+        }
+        public void setBarat(int x, int y)
+        {
+            this.B = new Tuple<int, int>(x,y);
+        }
+        public void setVisitNode(){
+            this.visited = true;
+        }
+        public void setUnvisitNode(){
+            this.visited = false;
+        }
+        // OTHER METHOD
+        public void addList(Node l)
+        {
+            this.path.Add(l.getInfo());
+            //this.path.Add(new Tuple<int, int, string>(l.getCoor().Item1, l.getCoor().Item2, l.name));
+
+        }
+        public void printNode()
+        {
+            Console.WriteLine("{0} - x : {1}, y : {2}", name, coor.Item1, coor.Item2);
+            if (this.U != null) Console.WriteLine("\tU - x : {0}, y : {1}", U.Item1, U.Item2);
+            else Console.WriteLine("\tU - null");
+            if (this.S != null) Console.WriteLine("\tS - x : {0}, y : {1}", S.Item1, S.Item2);
+            else Console.WriteLine("\tS - null");
+            if (this.T != null) Console.WriteLine("\tT - x : {0}, y : {1}", T.Item1, T.Item2);
+            else Console.WriteLine("\tT - null");
+            if (this.B != null) Console.WriteLine("\tB - x : {0}, y : {1}", B.Item1, B.Item2);
+            else Console.WriteLine("\tB - null");
+        }
+    }
+}
